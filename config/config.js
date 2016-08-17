4d7896de6076becaa31d9251954fcd83daea086c
HD Koh <hd.koh290293@gmail.com>
2016-08-17 18:29:42 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 // default app configuration
 module.exports = {
-  db: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || "mongodb://localhost/nodeProject",
+  db: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || "mongodb://admin:admin9309@ds139675.mlab.com:39675/nodeproject993",
   port: process.env.PORT || "3000",
   testDb: "mongodb://localhost/test",
   secret: "bukitPanjang998"
