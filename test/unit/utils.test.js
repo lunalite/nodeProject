61ee99f5f2aa7d43a601a05c1c44584ab1baaa4e
luna <hd.koh290293@gmail.com>
2016-08-24 12:13:47 +0200

    devel/1.8.2 removed log, edited test for utils

--- test/unit/utils.test.js
+++ test/unit/utils.test.js
@@ -12,11 +12,6 @@ describe('GET /nonExistentPages', function() {
     it('should return a not found page with status code 404', function(done) {
         request(app)
             .get('/a')
-            .expect(function(res) {
-                console.log("This is the res body:" + JSON.stringify(res.body));
-                console.log("This is the res:" + JSON.stringify(res));
-                res.body.message.should.equal("Not Found");
-            })
             .expect(404, done);
     });
     //TODO Add more nonsensical pages
